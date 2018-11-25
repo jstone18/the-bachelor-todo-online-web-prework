752014e21d53a291464288dfb3e91ccf9f3304ff
Jason Stone <jason.stone0808@gmail.com>
2018-11-25 19:17:42 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,7 @@ def get_first_name_of_season_winner(data, season)
     people.each do |k, v|
       if v == "Winner"
         full_name = people["name"]
-        return full_name.split(' ')
+        return full_name.split(' ').first
       end
     end
   end
