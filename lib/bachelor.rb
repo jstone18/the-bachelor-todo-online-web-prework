7491985ff20613e3b7aae980b5d10f2df8a7acaf
Jason Stone <jason.stone0808@gmail.com>
2018-11-25 19:50:45 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -57,7 +57,7 @@ def get_average_age_for_season(data, season)
     hash.each do |k, v|
       if k == "age"
         count += 1
-        age += v. to_f
+        age += v.to_f
       end
     end
   end
